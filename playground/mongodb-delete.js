86d6349647944260de066d8e87b3bf37a5c1aea8
Katawut Utaipaisalwong <katawut@apples-iMac.local>
2018-01-08 18:12:18 +0700

    setup test suite

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,20 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // deleteMany
+    db.collection('Todos').deleteMany({text: 'Eat protein'}).then((result) => {
+        console.log(result);
+    });
+
+    // deleteOne
+
+    // findOneAndDelete
+
+    //db.close();
+});
